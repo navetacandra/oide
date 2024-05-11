66a9d1135c11896d26dacb95a585527bb55501f4
navetacandra <naveta.cand@gmail.com>
2024-05-11 11:33:36 +0700

    add connection to database

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
   "fmt"
   "github.com/navetacandra/oide/lib/server"
   "net/http"
+  "database/sql"
+  _ "github.com/lib/pq"
 )
 
 const domain = "localhost"
 
+func GetConnection(host string, port int, user string, password string, dbname string) *sql.DB {
+  db, err := sql.Open(
+    "postgres", 
+    fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname),
+  )
+  if err != nil {
+    panic(err)
+  }
+  return db
+}
+
 func main() {
+  db := GetConnection("localhost", 5432, "postgres", "postgres", "oide")
+
   http.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
     server.HandleSubdomain(domain, "", w, r, func(w http.ResponseWriter, r *http.Request) {
       server.HandlePath("/", w, r, func(w http.ResponseWriter, r *http.Request) {   
