a51429d42244b87f9ee810d6d37881d7882e3326
navetacandra <naveta.cand@gmail.com>
2024-05-11 08:31:30 +0700

    create simple http proxy

--- lib/server/server.go
+++ lib/server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
-  "strings"
-  "net/http"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 func ParseSubdomain(domain string, r *http.Request) string {
@@ -23,5 +27,42 @@ func HandleSubdomain(domain string, subdomain string, w http.ResponseWriter, r *
 func HandlePath(path string, w http.ResponseWriter, r *http.Request, handler func(w http.ResponseWriter, r *http.Request)) {
   if r.URL.Path == path {
     handler(w, r)
+  }
+}
+
+func HandleProxy(w http.ResponseWriter, r *http.Request, targetHost string) {
+  client := &http.Client{Timeout: 30 * time.Second}
+  
+  path := r.URL.Path
+  if r.URL.RawQuery != "" {
+    path = path + "?" + r.URL.RawQuery
+  }
+  targetURL := fmt.Sprintf("%s%s", targetHost, path)
+
+  req, err := http.NewRequest(r.Method, targetURL, r.Body)
+  if err != nil {
+    log.Printf("Error preparing request [%s]: %v", targetURL, err)
+    http.Error(w, "Fail preparing request", http.StatusInternalServerError)
+    return
+  }
+
+  req.Header = r.Header
+  response, err := client.Do(req)
+  if err != nil {
+    log.Printf("Error forwarding request [%s]: %v", targetURL, err)
+    http.Error(w, "Fail forwarding request", http.StatusInternalServerError)
+    return
+  }
+  defer response.Body.Close()
+
+  for k, v := range response.Header {
+    for _, vv := range v {
+      w.Header().Add(k, vv)
+    }
+  }
+  w.WriteHeader(response.StatusCode)
+  _, err = io.Copy(w, response.Body)
+  if err != nil {
+    log.Printf("Error copying response [%s]: %v", targetURL, err)
   }
 }
--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
   "net/http"
 )
 
+const domain = "localhost"
+
 func main() {
   http.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
-    server.HandleSubdomain("localhost", "", w, r, func(w http.ResponseWriter, r *http.Request) {
+    server.HandleSubdomain(domain, "", w, r, func(w http.ResponseWriter, r *http.Request) {
       server.HandlePath("/", w, r, func(w http.ResponseWriter, r *http.Request) {   
         fmt.Fprintf(w, "Hello, World!\nAccess in: %s", server.ParseSubdomain("localhost", r))
       })
       server.HandlePath("/hello", w, r, func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, World!")
       })
+    })
+    server.HandleSubdomain(domain, "proxy", w, r, func(w http.ResponseWriter, r *http.Request) {
+      server.HandleProxy(w, r, "http://localhost:8080")
     })
   })
 
